babf256f1b9542237eaa31b17726724117c362c9
ddwugu <136143092+ddwugu@users.noreply.github.com>
2023-12-29 19:46:10 +0700

    Stream_data.py

--- Stream_data.py
+++ Stream_data.py
@@ -7,7 +7,7 @@ import streamlit as st
 LokasiKM = pickle.load(open('Pred_lokasi.sav', 'rb'))
 
 # Judul Web
-st.title('Finding Oil Losses Pertamina Field Jambi')
+st.title('Pertamina Field Jambi')
 
 Titik_1_PSI = st.text_input('Input Pressure di titik 1 (PSI)')
 Titik_2_PSI = st.text_input('Input Pressure di titik 2 (PSI)')
@@ -16,14 +16,14 @@ Titik_2_PSI = st.text_input('Input Pressure di titik 2 (PSI)')
 suspect_loct = ''
 
 # Tombol prediksi
-if st.button('Prediksi Lokasi Kebocoran'):
+if st.button('Prediksi Lokasi'):
     prediksi_lokasi = LokasiKM.predict([[float(Titik_1_PSI), float(Titik_2_PSI)]])
 
     if prediksi_lokasi[0] == 0:
-        suspect_loct = 'Tidak Terdapat Kebocoran'
+        suspect_loct = 'Pipa Aman'
     elif prediksi_lokasi[0] == 26.8:
         suspect_loct = 'Tidak Terdapat Fluida yang Mengalir'
     else:
-        suspect_loct = f'Terjadi Kebocoran di titik {prediksi_lokasi[0]} KM'
+        suspect_loct = f'Terjadi di titik {prediksi_lokasi[0]} KM'
 
     st.success(suspect_loct)
